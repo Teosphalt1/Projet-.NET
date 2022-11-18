668e3da484ae18538cdcf0ea16947f09f98cabd9
Teosphalt1 <theophile.mariotte2204@gmail.com>
2022-11-18 16:34:18 +0100

    prosit 2

---
+++ Prosit 2/Workshop/trainingWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trainingWPF
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ICommand m_oneCommand;
+        public ICommand oneCommand
+        {
+            get { return m_oneCommand; }
+            set { m_oneCommand = value;}
+        }
+
+        public MainWindowViewModel()
+        {
+            oneCommand = new RelayCommand(new Action<object>(InsertValue));
+        }
+
+        public void InsertValue(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
