8543832cacc55ad80c7afbc279abaac99796a4fb
Teosphalt1 <theophile.mariotte2204@gmail.com>
2022-11-19 17:36:53 +0100

    pattern

---
+++ Prosit 3/StrategyPatternFR-ENG/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternFR_ENG
+{
+    public interface IStrategy
+    {
+        void DoAlgorithm();
+        void ExecuteASave();
+        void AddASave();
+        void ExecuteOnAllTheSaves();
+        void ExecuteOnASpecificSave();
+        void AllSaveComplete();
+        void AllSaveDifferential();
+        void SpecificSaveComplete();
+        void SpecificSaveDifferential();
+    }
+}
